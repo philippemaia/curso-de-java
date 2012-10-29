bf67e679ca8eff075c780898637b1e7744ef180c
Philippe <philippe@philippe-HP-Pavilion-g4-Notebook-PC.(none)>
2012-10-29 17:26:45 -0400

    classe Pessoa

---
+++ estagiarios/philippe-maia/Exercicios/src/Pessoa.java
@@ -0,0 +1,10 @@
+
+public class Pessoa {	
+	
+	public static void main(String[] args) {
+		Pessoa pessoa[] = new Pessoa[4];
+		for(int i = 0; i<pessoa.length;i++){
+			System.out.println(pessoa);
+		}
+	}
+}
